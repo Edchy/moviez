4e8d72f67f21d17c2b5892d04d21dca65828f762
amircoll1235 <amir_abukar@outlook.com>
2024-12-12 12:49:39 +0100

    review

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+
+import "./style.css";
+ const textField = document.createElement('input');
+ textField.type = 'text';
+ textField.placeholder = 'Enter text here';
+
+ document.body.appendChild(textField);
+
+ const reviewBox = document.createElement('divbox');
+ reviewBox.id = 'reviewBox';
+ reviewBox.textContent = 'Reviews:';
+ document.body.appendChild(reviewBox);
+
+
+
